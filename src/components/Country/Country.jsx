cc0d2b91a3dbef15e25985d9e73c44b18f151b33
PhantomOblivion <smasif404@gmail.com>
2026-03-15 09:00:28 +0600

    Add-Show Flags, and Handle Data Error (33-3)

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Country = () => {
-    return (
-        <div>
-            <h3>Name: </h3>
-        </div>
-    );
+const Country = ({ country }) => {
+  console.log(country.continents.continents);
+  return (
+    <div>
+      <h3>Name: {country.name.common}</h3>
+      <p>Official Name: {country.name.official}</p>
+      <p>
+        Currencies:
+        {Object.keys(country.currencies.currencies)
+          .map((key) => {
+              const currency = country.currencies.currencies[key];
+              return `${currency.name} (${currency.symbol})`;
+            })
+            .join(", ")}
+      </p>
+      <p>Capital: {country.capital.capital}</p>
+      <p>Region: {country.region.region}</p>
+      <p>
+        Languages:
+        {Object.values(country.languages.languages).join(", ")}
+      </p>
+      <p>Area: {country.area.area}</p>
+      <p>Popultaion: {country.population.population}</p>
+      <p>Continents: {country.continents.continents}</p>
+      <img src={country.flags.flags.png} alt="{country.flags.flags.alt}" />
+    </div>
+  );
 };
 
 export default Country;
